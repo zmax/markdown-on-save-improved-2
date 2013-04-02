c22f4cd05a10d66a0ca3f31fdda9ce4ae9fc96ce
Zmax <zenyer@gmail.com>
2013-04-03 06:21:55 +0800

    project init.

--- README.md
+++ README.md
@@ -1,4 +1,240 @@
-markdown-on-save-improved-2
-===========================
+# Markdown on Save Improved 2
 
 WordPress + Markdown + EpicEditor, and it support Github fenced code block. (It required with Crayon syntax highlighter plugin)
+
+Contributors: Starck Lin  
+Tags: markdown, formatting, mobile  
+Requires at least: 3.3  
+Tested up to: 3.5  
+Stable tag: 1.2   
+
+## Changelog
+
+### 2.5.3
+
+- Auto save to WP-Editor content
+
+### 2.5.2 
+
+- Improved "Toggle EpicEditor"
+
+### 2.5
+
+- Support Github Fenced code blocks
+- Integrated with [EpicEditor](https://github.com/OscarGodson/EpicEditor) v0.2.0
+- Compatible with [Crayon](https://github.com/aramkocharyan/crayon-syntax-highlighter) WP-Plugin
+
+
+# Markdown on Save Improved #
+Contributors: mattwiebe  
+Tags: markdown, formatting, mobile  
+Requires at least: 3.2  
+Tested up to: 3.5.1  
+Stable tag: 2.4.1  
+
+Markdown + WordPress = writing bliss.
+
+## Description
+
+WordPress will process your post with Markdown unless you tell it not to. You probably won't want to, since writing in Markdown is so awesome. Compatible with mobile apps and 3rd-party blogging tools.
+
+The main difference between this plugin and the [original](http://wordpress.org/extend/plugins/markdown-on-save/) is that this plugin assumes you always want Markdown processing.
+
+The markdown version is stored separately (in the `post_content_formatted` column), so you can deactivate this plugin and your posts won't spew out Markdown, because HTML is stored in the `post_content` column, just like normal. This is also much faster than doing on-the-fly Markdown conversion on every page load. It's only done once! When you re-edit the post, the markdown version is swapped into the editor for you to edit.
+
+## Installation
+
+1. Upload the `markdown-on-save-improved` folder to your `/wp-content/plugins/` directory
+2. Activate the "Markdown On Save Improved" plugin in your WordPress administration interface
+3. Create a new post with Markdown.
+4. Your post can be edited using Markdown, but will save as processed HTML on the backend.
+
+## Screenshots
+
+1. The meta box where you can disable Markdown formatting or convert Markdown to HTML.
+
+## Frequently Asked Questions
+
+### How do I use Markdown syntax?
+
+Please refer to this resource: [PHP Markdown Extra](http://michelf.com/projects/php-markdown/extra/).
+
+### How do I disable Markdown processing?
+
+In most cases this should be unnecessary, since Markdown ignores existing HTML. But if Markdown for some reason disturbs your HTML when making an edit to a non-Markdown post, either check the "Disable Markdown formatting" checkbox, or put a `<!--no-markdown-->` HTML comment in your post somewhere (useful when using a mobile or 3rd party blogging app).
+
+### How do I enable Markdown processing on a custom post type?
+
+Just add `add_post_type_support( 'your-post-type', 'markdown-osi' );` to your [functionality plugin](http://wpcandy.com/teaches/how-to-create-a-functionality-plugin/) `functions.php` file. Note that it must be hooked to the `init` action like this:
+
+    add_action( 'init', 'your_prefix_add_markdown_support' );
+    function your_prefix_add_markdown_support(){
+        add_post_type_support( 'your-post-type', 'markdown-osi' );
+    }
+
+Note that posts and pages are supported by default.
+
+### How do I convert an existing post to Markdown? 
+
+There is an experimental checkbox in the post editor to convert your old HTML post to Markdown using [Markdownify](http://milianw.de/projects/markdownify/). **Check it at your own risk.** Make sure you have revisions on or backups. If you're relying on specifically crafted HTML, it might get destroyed. But it might be cool.
+
+### What if I don't want to see that Markdown meta box at all?
+
+I see you love Markdown greatly. You have chosen wisely. Add the following constant to your wp-config.php file:
+
+    define( 'SD_HIDE_MARKDOWN_BOX', true );
+
+Note that you can still disable Markdown formatting with a `<!--no-markdown-->` HTML comment. HTML to Markdown conversion will be impossible.
+
+### What happens if I decide I don't want this plugin anymore?
+
+Just deactivate it. The Markdown version is stored separately, so without the plugin, you'll just revert to editing the HTML version.
+
+## Changelog
+
+### 2.4.1
+
+* Compatibility with other plugins that might load Markdown Extra (such as [GitHub-Flavored Markdown Comments](http://wordpress.org/extend/plugins/github-flavored-markdown-comments/))
+
+### 2.4
+
+* Require `add_post_type_support( 'post_type', 'markdown-osi' );` to enable Markdown on custom post types. This makes it play nicely with Jetpack's Custom CSS module, which stores your CSS in a custom post type.
+
+### 2.3
+
+* Update to Markdown Extra 1.2.6 [Markdown Extra release notes](http://michelf.ca/projects/php-markdown/#version-history)
+* Add relevant `wp.*` XML-RPC methods for full remote posting/editing capability
+* Remote posting fixes
+* Consistent WordPress coding style
+
+### 2.2
+
+* Better backwards-compatibility for non-Markdown posts that were written before installing this plugin. (i.e. don't turn off wpautop except when needed.)
+
+### 2.1
+
+* Favour Markdown formatting in all cases, turn off wpautop. Ensures that paragraphs and line breaks are handled according to the [Markdown documentation](http://daringfireball.net/projects/markdown/syntax#p).
+* Maintain oEmbed auto-embedding
+
+### 2.0.1
+
+* Disable visual editor when editing Markdown
+
+### 2.0
+
+* Move to opt-out model
+* Add experiemental HTML to Markdown conversion using [Markdownify](http://milianw.de/projects/markdownify/)
+
+### 1.0.2
+
+* Fix bug where desired backslashes went missing
+
+### 1.0.1
+
+* Add stripslashes to let Markdown links with titles get parsed.
+
+### 1.0
+
+* Initial forked release
+
+### Upgrade Notice
+
+### 1.0.1
+* The next release will move to an opt-out, rather than opt-in approach, as Mark Jaquith has merged the XML-RPC stuff back into [Markdown on Save](http://http://wordpress.org/extend/plugins/markdown-on-save/)
+
+
+
+# Markdown on Save #
+Contributors: markjaquith  
+Donate link: http://txfx.net/wordpress-plugins/donate  
+Tags: markdown, formatting  
+Requires at least: 3.3  
+Tested up to: 3.5  
+Stable tag: 1.2  
+
+Allows you to compose content in Markdown on a per-item basis. The markdown version is stored separately, so you can deactivate this plugin any time.
+
+## Description ##
+
+This plugin allows you to compose content in Markdown on a per-item basis. The markdown version is stored separately (in the `post_content_formatted` column), so you can deactivate this plugin and your posts won't spew out Markdown, because HTML is stored in the `post_content`, just like normal. This is also much faster than doing on-the-fly Markdown conversion on every page load. It's only done once! When you re-edit the post, the markdown version is swapped into the editor for you to edit. If something external updates the post content, you'll lose the Markdown version.
+
+## Installation ##
+
+1. Upload the `markdown-on-save` folder to your `/wp-content/plugins/` directory
+
+2. Activate the "Markdown On Save" plugin in your WordPress administration interface
+
+3. Create a new post with Markdown, and check the "This post is formatted with Markdown" box.
+
+4. Done! Now that post can be edited using Markdown, but will save as processed HTML on the backend.
+
+## Screenshots ##
+
+1. The meta box where you designate a post as containing Markdown. This is the only UI for the plugin!
+
+## Frequently Asked Questions ##
+
+### How do I use Markdown syntax? ###
+
+Please refer to this resource: [http://michelf.com/projects/php-markdown/extra/](PHP Markdown Extra).
+
+### What happens if I uncheck the Markdown box? ###
+
+Your post will no longer be interpreted as Markdown, and you may have to alter the post to remove Markdown formatting.
+
+### What happens if I decide I don't want this plugin anymore? ###
+
+Just deactivate it. The Markdown version is stored separately, so without the plugin, you'll just revert to editing the HTML version.
+
+## Changelog ##
+### 1.2 ###
+* Update PHP Markdown Extra to 1.2.6
+* Keep track of which revisions were Markdown and which were not
+* Restore old revisions properly, including Markdown status
+* Fix a slashing bug that would prevent link titles from parsing
+* Fix a bug related to autosave
+* Use Dustin Curtis' Markdown logo as a toggle
+* Work on the WP-specific XML-RPC API in addition to the generic API
+
+### 1.1.5 ###
+* Fix a `stripslashes()` error
+
+### 1.1.4 ###
+* XML-RPC support (use <!--markdown--> to enable Markdown mode)
+
+### 1.1.3 ###
+* Disables the Visual Editor if the post being edited is in Markdown mode
+
+### 1.1.2 ###
+* Fix a slashes bug which would cause link titles to fail
+* Enable Markdown when posting remotely by using <!--markdown--> anywhere in post content
+
+### 1.1.1 ###
+* Fix bug which made the metabox show up on the Dashboard
+
+### 1.1 ###
+* Some extra nonce protection
+* Enable the plugin for all content types, not just posts
+
+### 1.0 ###
+* First public version
+* Fixed a regex bug that could break current menu highlighting. props skarab
+
+## Upgrade Notice ##
+### 1.1.5 ###
+Update to fix issues with slashes disappearing.
+
+### 1.1.4 ###
+Upgrade to use Markdown over XML-RPC (use <!--markdown--> to enable it)
+
+### 1.1.3 ###
+Upgrade to fix the bug that caused "titles" in links to fail parsing and to disable the visual editor in Markdown mode
+
+### 1.1.2 ###
+Upgrade to fix the bug that caused "titles" in links to fail parsing
+
+### 1.1.1 ###
+Prevents the meta box from mistakenly appearing on the Dashboard
+
+### 1.1 ###
+Enables the Markdown option for all content types, instead of limiting it to posts
---
+++ markdown-on-save.php
@@ -0,0 +1,405 @@
+<?php
+/*
+Plugin Name: Markdown on Save Improved 2
+Description: Allows you to compose content in Markdown on a per-item basis from WP admin or mobile/3rd-party apps. The markdown version is stored separately, so you can deactivate this plugin and your posts won't spew out Markdown. Based on <a href="http://wordpress.org/extend/plugins/markdown-osi/">Mark Jaquith's plugin</a>. It supports Github Fenced code blocks, and integrated with <a href="http://epiceditor.com/">EpicEditor</a>.
+Version: 2.5.3
+
+Author: Starck Lin
+Author URI: http://blog.starcklin.com
+License: GPL v2
+*/
+
+/*
+ * Copyright 2012-2013 Starck Lin. GPL v2, of course.
+ *
+ * This software is forked from the original Markdown on Save improved plugin (c) Matt Wiebe
+ * It uses the Markdown Extra and Markdownify libraries. Copyrights and licenses indicated in said libararies.
+ *
+ *
+ * This software is forked from the original Markdown on Save plugin (c) Mark Jaquith
+ * It uses the Markdown Extra and Markdownify libraries. Copyrights and licenses indicated in said libararies.
+ *
+ */
+
+if(!function_exists('get_plugin_data')) {
+ 	require_once('wp-includes/plugin.php');
+}
+
+// Define the default path and URL for the WP Editor plugin
+define('EPICEDITOR_PATH', plugin_dir_path( __FILE__ ) . '/epiceditor/' );
+define('EPICEDITOR_URL', plugins_url() . '/' . basename(dirname(__FILE__)) . '/epiceditor/' );
+define('EPICEDITOR_VERSION', '0.2.0');
+
+class SD_Markdown {
+
+	const PM = '_sd_disable_markdown';
+	const MD = '_sd_is_markdown';
+	const CONVERT = 'sd_convert_to_markdown';
+	const VERSION = '2.5.3';
+	const VERSION_OPT = 'mosi-version';
+
+	protected $new_api_post = false;
+
+	protected $parser = false;
+
+	public function __construct() {
+		add_action( 'init', array( $this, 'init' ) );
+		register_activation_hook( __FILE__, array($this, 'activate') );
+	}
+
+	public function init() {
+
+		load_plugin_textdomain( 'markdown-osi', NULL, basename( dirname( __FILE__ ) ) );
+
+		$this->add_post_type_support();
+
+		add_action( 'do_meta_boxes', array( $this, 'do_meta_boxes' ), 20, 2 );
+		add_action( 'xmlrpc_call', array($this, 'xmlrpc_actions') );
+		add_action( 'load-post.php', array( $this, 'load' ) );
+		add_action( 'load-post-new.php', array( $this, 'load' ) );
+		add_action( 'xmlrpc_call_success_mw_newPost', array( $this, 'xmlrpc_new_post' ), 10, 2 );
+
+		remove_action( 'the_content', 'wpautop' );
+
+		add_filter( 'wp_insert_post_data', array( $this, 'wp_insert_post_data' ), 10, 2 );
+		add_filter( 'edit_post_content', array( $this, 'edit_post_content' ), 10, 2 );
+		add_filter( 'edit_post_content_filtered', array( $this, 'edit_post_content_filtered' ), 10, 2 );
+		add_filter( 'the_content', array($this, 'maybe_default_formatting' ) );
+
+		// Markdown breaks autoembedding by wrapping URLs on their own line in paragraphs
+		if ( get_option( 'embed_autourls' ) )
+			add_filter( 'the_content', array($this, 'oembed_fixer' ), 8 );
+
+		if ( defined( 'XMLRPC_REQUEST') && XMLRPC_REQUEST )
+			$this->maybe_prime_post_data();
+      
+      	add_action( 'admin_init', array($this, 'epic_plugin_admin_init') );
+
+      	wp_register_script('epic_editor', EPICEDITOR_URL . '/js/epiceditor.js', false, EPICEDITOR_VERSION);
+        
+	}
+
+	public function epic_plugin_admin_init() {
+		wp_enqueue_script( 'epic_editor' );
+	}
+
+	protected function add_post_type_support() {
+		add_post_type_support( 'post', 'markdown-osi' );
+		add_post_type_support( 'page', 'markdown-osi' );
+	}
+
+	public function maybe_default_formatting( $content ) {
+		if ( ! post_type_supports( get_post_type(), 'markdown-osi' ) || ! $this->is_markdown( get_the_ID() ) )
+			$content = wpautop( $content );
+
+		return $content;
+	}
+
+	public function xmlrpc_new_post( $post_id, $args ) {
+		$this->new_api_post = true;
+		remove_filter( 'wp_insert_post_data', array( $this, 'wp_insert_post_data' ), 10, 2 );
+		$post = (array) get_post( $post_id );
+		$post = $this->wp_insert_post_data( $post, $post );
+
+		wp_update_post( $post );
+	}
+
+	public function xmlrpc_actions( $xmlrpc_method ) {
+		$make_filterable = array( 'metaWeblog.getRecentPosts', 'wp.getPosts', 'wp.getPages' );
+
+		if ( in_array( $xmlrpc_method, $make_filterable ) )
+			add_action( 'parse_query', array($this, 'make_filterable'), 10, 1 );
+	}
+
+	// we have to do it early and ghetto like this since metaWeblog.getPost && wp.getPage
+	// fire *after* get_post is called in their methods
+	public function maybe_prime_post_data() {
+		global $HTTP_RAW_POST_DATA;
+		require_once( ABSPATH . WPINC . '/class-IXR.php' );
+		$message = new IXR_Message( $HTTP_RAW_POST_DATA );
+		if ( ! $message->parse() ) {
+			unset( $message );
+			return;
+		}
+
+		$methods_to_prime = array( 'metaWeblog.getPost', 'wp.getPost', 'wp.getPage' );
+		if ( ! in_array( $message->methodName, $methods_to_prime ) ) {
+			unset( $message );
+			return;
+		}
+
+		// different ID arg for wp.getPage
+		$post_id = ( 'wp.getPage' === $message->methodName ) ? $message->params[1] : array_shift( $message->params );
+		$post_id = (int) $post_id;
+		// prime the post cache
+		if ( $this->is_markdown( $post_id ) ) {
+			$post = get_post( $post_id );
+			if ( ! empty( $post->post_content_filtered ) )
+				$post->post_content = $post->post_content_filtered;
+			wp_cache_delete( $post->ID, 'posts' );
+			wp_cache_add( $post->ID, $post, 'posts' );
+		}
+		unset( $message );
+	}
+
+	public function make_filterable( $wp_query ) {
+		$wp_query->set( 'suppress_filters', false );
+		add_action( 'the_posts', array( $this, 'the_posts' ), 10, 2 );
+	}
+
+	public function the_posts($posts, $wp_query) {
+		foreach ( $posts as $key => $post ) {
+			if ( $this->is_markdown($post->ID) )
+				$posts[ $key ]->post_content = $posts[ $key ]->post_content_filtered;
+		}
+		return $posts;
+	}
+
+	public function load() {
+		if ( ! ( isset( $_GET['post'] ) && ! $this->is_markdown( $_GET['post'] ) ) )
+			add_filter( 'user_can_richedit', '__return_false', 99 );
+	}
+
+	public function wp_insert_post_data( $data, $postarr ) {
+		// run once
+		remove_filter( 'wp_insert_post_data', array( $this, 'wp_insert_post_data' ), 10, 2 );
+
+		// checks
+		$nonced = ( isset( $_POST['_sd_markdown_nonce'] ) && wp_verify_nonce( $_POST['_sd_markdown_nonce'], 'sd-markdown-save' ) );
+		$disable_ticked = ( $nonced && isset( $_POST['sd_disable_markdown'] ) );
+		$disable_comment_inserted = ( false !== stripos( $data['post_content'], '<!--no-markdown-->' ) );
+		$do_html_to_markdown = ( $nonced && isset($_POST[self::CONVERT]) );
+		$id = ( isset( $postarr['ID'] ) ) ? $postarr['ID'] : 0;
+
+		$supports = post_type_supports( $postarr['post_type'], 'markdown-osi' );
+
+		// double check in case this is a new xml-rpc post. Disable couldn't be checked.
+		if ( $this->new_api_post )
+			$disable_ticked = false;
+
+		// Maybe do HTML --> Markdown
+		if ( $do_html_to_markdown )
+			$data['post_content'] = $this->html_to_markdown( $data['post_content'] );
+
+		// Make sure markdown processing isn't disabled for this post
+		if ( $supports && ! ( $disable_ticked || $disable_comment_inserted ) ) {
+          
+          	// 將原始內容存至 post_content_filtered
+			$data['post_content_filtered'] = $data['post_content'];
+          
+          	// Do markdown processing
+          	$data['post_content'] = $this->process( $data['post_content'], $id );
+          
+			if ( $id )
+				update_post_meta( $id, self::MD, 1 );
+		} else {
+			$data['post_content_filtered'] = '';
+			if ( $id )
+				update_post_meta( $id, self::MD, false );
+		}
+
+		return $data;
+	}
+
+	/**
+	 * Fixes oEmbed auto-embedding of single-line URLs
+	 *
+	 * WP's normal autoembed assumes that there's no <p>'s yet because it runs before wpautop
+	 * But, when running Markdown, we have <p>'s already there, including around our single-line URLs
+	 */
+	public function oembed_fixer( $content ) {
+		global $wp_embed;
+		return preg_replace_callback( '|^\s*<p>(https?://[^\s"]+)</p>\s*$|im', array( $wp_embed, 'autoembed_callback' ), $content );
+	}
+
+	protected function html_to_markdown( $content ) {
+		$content = stripslashes( $content );
+		$content = wpautop( $content );
+		require_once( dirname(__FILE__) . '/markdownify/markdownify_extra.php' );
+		$md = new Markdownify_Extra( true );
+		$content = $md->parseString( $content );
+		return $content;
+	}
+
+	protected function process( $content, $id ) {
+		$this->maybe_load_markdown();
+		// Links with "quoted titles" bork without stripping slashes.
+		// Stripslashes would lose desired backslashes. So, regex.
+		$content = preg_replace( '/\\\"/', '"', $content );
+      
+      	// Added by Starck Lin - parsing Github Fenced code blocks
+      	// /^ *``` *(\w+)? *\n([^\0]+?)\s*``` *(?:\n+|$)/
+        $content = preg_replace("/\n```([A-z :]+)([^```]+?)\n```/", '<pre class="lang:$1">$2</pre>', $content );
+        //$content = preg_replace("/\s*```\s*(\w+)?\s*\n([^\0]+?)\s*```\s*(?:\n+|$)/", '<pre class="lang:$1">$2</pre>', $content );
+        $content = preg_replace("/\n```([^```]+?)\n```/", '<pre>$1</pre>', $content );
+      	
+		// convert to Markdown
+      	$content = $this->parser->transform( $content );
+      
+		// reference the post_id to make footnote ids unique
+		$content = preg_replace( '/fn(ref)?:/', "fn$1-$id:", $content );
+      	
+      	return $content;
+	}
+
+	protected function maybe_load_markdown() {
+		// In case another plugin has included it - hopefully it's compatible
+		if ( ! class_exists( 'MarkdownExtra_Parser' ) )
+			require_once( dirname( __FILE__ ) . '/markdown-extra/markdown-extra.php' );
+		if ( ! $this->parser )
+			$this->parser = new MarkdownExtra_Parser;
+	}
+
+	public function do_meta_boxes( $type, $context ) {
+		// allow disabling for folks who think markdown should always be on.
+		if ( defined( 'SD_HIDE_MARKDOWN_BOX') && SD_HIDE_MARKDOWN_BOX )
+			return;
+
+		if ( 'side' == $context && in_array( $type, array_keys( get_post_types() ) ) )
+			add_meta_box( 'sd-markdown', __( 'Markdown', 'markdown-osi' ), array( $this, 'meta_box' ), $type, 'side', 'high' );
+	}
+
+	public function meta_box() {
+		global $post;
+		$screen = get_current_screen();
+		wp_nonce_field( 'sd-markdown-save', '_sd_markdown_nonce', false, true );
+		echo '<p><input type="checkbox" name="sd_disable_markdown" id="sd_disable_markdown" value="1" ';
+		// we get false positives on new post screens. Do not want.
+		if ( 'add' !== $screen->action )
+			checked( ! get_post_meta( $post->ID, self::MD, true ) );
+		echo ' /> <label for="sd_disable_markdown">' . __( 'Disable Markdown formatting', 'markdown-osi' ) . '</label></p>';
+		printf( '<p><label><input type="checkbox" name="%s" /> %s</label></p>', self::CONVERT, __('Convert HTML to Markdown (experimental)', 'markdown-osi') );
+
+      	$url = EPICEDITOR_URL;
+
+      	echo <<<EPIC_EDITOR
+
+<script>
+
+var epic_editor;
+
+function save_to_content() {
+	jQuery('#content').val( epic_editor.getElement('editor').body.innerText );
+}
+
+function toogleEpicEditor() {
+	if( epic_editor ) {
+		if( epic_editor.is('loaded') ) {
+			save_to_content();
+			jQuery('#wp-content-media-buttons').show();
+			jQuery('#ed_toolbar').show();
+			jQuery('#content').show();
+			// jQuery('#save-post').attr('disabled',false);
+			// jQuery('#publish').attr('disabled',false);
+			epic_editor.unload( function() {
+				jQuery('#epiceditor').css('height','0px');
+			} );
+		}else{
+			jQuery('#epiceditor').css('height', jQuery('#content').css('height') );
+			jQuery('#wp-content-media-buttons').hide();
+			jQuery('#ed_toolbar').hide();
+			jQuery('#content').hide();
+			// jQuery('#save-post').attr('disabled',true);
+			// jQuery('#publish').attr('disabled',true);
+			epic_editor.load( function(){
+				epic_editor.reflow();
+				//epic_editor.getElement('editor').body.innerHTML = jQuery('#content').val().replace( /[\\r]?\\n/g, "<br/>" );
+				epic_editor.getElement('editor').body.innerText = jQuery('#content').val();
+
+			} );
+		}
+	}
+}
+
+var opts = {
+  container: 'epiceditor',
+  basePath: '${url}',
+  clientSideStorage: true,
+  localStorageName: 'epiceditor',
+  useNativeFullsreen: true,
+  parser: marked,
+  file: {
+    name: 'epiceditor',
+    defaultContent: '',
+    autoSave: 100
+  },
+  theme: {
+    base:'/themes/base/epiceditor.css',
+    preview:'/themes/preview/github.css',
+    editor:'/themes/editor/epic-dark.css'
+  },
+  focusOnLoad: false,
+  shortcut: {
+    modifier: 18,
+    fullscreen: 70,
+    preview: 80
+  }
+}
+
+var epic_container = jQuery('<div id="epiceditor"></div>');
+epic_container.insertBefore('#content');
+
+epic_editor = new EpicEditor(opts);
+
+epic_editor.on('save', function(){
+	save_to_content();
+});
+
+</script>
+
+EPIC_EDITOR;
+
+		printf( '<p><lable><input type="button" value="%s" class="button button-large" onclick="toogleEpicEditor();return true;" /></label></p>', __('Toogle EpicEditor') );
+
+	}
+
+	private function is_markdown( $id ) {
+		return (bool) get_post_meta( $id, self::MD, true );
+	}
+
+	public function edit_post_content( $content, $id ) {
+		if ( $this->is_markdown( $id ) ) {
+			$post = get_post( $id );
+			if ( $post && ! empty( $post->post_content_filtered ) )
+				$content = $post->post_content_filtered;
+		}
+		return $content;
+	}
+
+	public function edit_post_content_filtered( $content, $id ) {
+		return $content;
+	}
+
+	public function activate() {
+		$previous_version = get_option( self::VERSION_OPT, '2.1' );
+		// upgrade to new determining of MD
+		if ( version_compare( '2.1', $previous_version, '=' ) ) {
+			$this->update_schema();
+		}
+		update_option( self::VERSION_OPT, self::VERSION );
+	}
+
+	/**
+	* Previously, we only set a meta value for disabling. Now we'll set one regardless. Old options need updating.
+	*/
+	private function update_schema() {
+		global $wpdb;
+		// formerly MD-disabled posts get updated to new meta key with false value
+		$query = $wpdb->prepare( "UPDATE {$wpdb->postmeta} SET meta_key = %s, meta_value = '' WHERE meta_key = %s ", self::MD, self::PM );
+		$wpdb->query( $query );
+
+		// posts with stuff in post_content_filtered get the new meta key with true value
+		$ids = $wpdb->get_col( "SELECT ID from {$wpdb->posts} WHERE post_content_filtered != '' " );
+		if ( $ids && ! empty( $ids ) ) {
+			foreach ( $ids as $id ) {
+				// reprocess markdown -> we used to strip <p> tags and rely on wpautop
+				$post = get_post( $id );
+				$post->post_content = $this->process( $post->post_content_filtered, $id );
+				wp_update_post( $post );
+				update_post_meta( $id, self::MD, 1 );
+			}
+		}
+	}
+}
+
+new SD_Markdown;
